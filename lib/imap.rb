2cf31f4ae788753539f1d6ba35540e355a65cf64
Conrad Irwin <conrad.irwin@gmail.com>
2011-04-14 00:03:18 -0700

    Start enthusiasticly fleshing the API

--- lib/imap.rb
+++ lib/imap.rb
@@ -10,8 +10,55 @@ module EventMachine
       @connection = connection
     end
 
+    def disconnect
+      @connection.close_connection
+    end
+
+    ## 6.1 Client commands - any state.
+
     def capability
       one_data_response("CAPABILITY")
+    end
+
+    def noop
+      tagged_response("NOOP")
+    end
+
+    # Logout and close the connection.
+    def logout
+      tagged_response("LOGOUT").errback do |e|
+        if e.is_a? Net::IMAP::ByeResponseError
+          # RFC 3501 says the server MUST send a BYE response and then close the connection.
+          disconnect
+          succeed
+        end
+      end.callback do |response|
+        fail Net::IMAP::ResponseParseError.new("Received the wrong response to LOGOUT: #{response}")
+      end
+    end
+
+    ## 6.2 Client commands - "Not authenticated" state
+
+    def starttls
+      raise NotImplementedError
+    end
+
+    def authenticate(auth_type, *args)
+      auth_type = auth_type.upcase
+      raise "bleargh"
+    end
+
+
+    def create(mailbox)
+      tagged_response("CREATE")
+    end
+
+    def delete(mailbox)
+      tagged_response("DELETE")
+    end
+
+    def examine(mailbox)
+      tagged_response("EXAMINE")
     end
 
     def login(username, password)
@@ -22,14 +69,17 @@ module EventMachine
       tagged_response("LOGOUT")
     end
 
-    def noop
-      tagged_response("NOOP")
-    end
-
     def select(mailbox)
       tagged_response("SELECT", mailbox)
     end
 
+    def subscribe(mailbox)
+      send_command("SUBSCRIBE", mailbox)
+    end
+
+    def rename(mailbox, newname)
+      tagged_response("RENAME", mailbox, newname)
+    end
 
     private
     
