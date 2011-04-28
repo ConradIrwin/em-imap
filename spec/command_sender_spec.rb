549af9556c6a6ec42042e885a3a2cad7c082e9ae
Conrad Irwin <conrad.irwin@gmail.com>
2011-04-28 14:48:17 -0700

    Add some more specs for the command sending

--- spec/command_sender_spec.rb
+++ spec/command_sender_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe EM::Imap::CommandSender do
 
+  before :each do
+    @command_sender = Class.new(EMStub) do
+      include EM::Imap::Connection
+    end.new
+  end
+
   describe "#send_authentication_data" do
 
     before :each do
-      @command_sender = Class.new(EMStub) do 
-        include EM::Imap::Connection
-      end.new
-
       @authenticator = Class.new do
         def process; end
       end.new
@@ -63,6 +65,92 @@ describe EM::Imap::CommandSender do
       }.from(true).to(false)
     end
 
+  end
+
+  describe "#send_literal" do
+    before :each do
+      @command = EM::Imap::Command.new("RUBY0001", "SELECT", ["AHLO"])
+    end
+
+    it "should initially only send the size" do
+      @command_sender.should_receive(:send_data).with("{4}\r\n")
+      @command_sender.send_literal "AHLO", @command
+    end
+
+    it "should send the remainder after the continuation response" do
+      @command_sender.should_receive(:send_data).with("{4}\r\n")
+      @command_sender.send_literal "AHLO", @command
+      @command_sender.should_receive(:send_data).with("AHLO")
+      @command_sender.receive_data "+ Continue\r\n"
+    end
+
+    it "should pause the sending of all the other literals" do
+      @command_sender.should_receive(:send_data).with("SELECT {4}\r\n")
+      @command_sender.send_string "SELECT ", @command
+      @command_sender.send_literal "AHLO", @command
+      @command_sender.send_string "\r\n", @command
+      @command_sender.should_receive(:send_data).with("AHLO")
+      @command_sender.should_receive(:send_data).with("\r\n")
+      @command_sender.receive_data "+ Continue\r\n"
+    end
+  end
+
+  describe "#send_command_object" do
+    before :each do
+      @bomb = Object.new
+      def @bomb.send_data(connection)
+        raise "bomb"
+      end
+    end
+
+    it "should raise errors if the command cannot be serialized" do
+      lambda {
+        @command_sender.send_command_object(EM::Imap::Command.new("RUBY0001", "IDLE", [@bomb]))
+      }.should raise_exception "bomb"
+    end
+
+    it "should raise errors even if the unserializable object is after a literal" do
+      lambda {
+        @command_sender.send_command_object(EM::Imap::Command.new("RUBY0001", "IDLE", ["Literal\r\nString", @bomb]))
+      }.should raise_exception "bomb"
+    end
+
+    it "should not raise errors if the send_data fails" do
+      @command_sender.should_receive(:send_data).and_raise(Errno::ECONNRESET)
+      lambda {
+        @command_sender.send_command_object(EM::Imap::Command.new("RUBY0001", "IDLE"))
+      }.should_not raise_exception
+    end
+
+    it "should fail the command if send_data fails" do
+      @command_sender.should_receive(:send_data).and_raise(Errno::ECONNRESET)
+      a = []
+      command = EM::Imap::Command.new("RUBY0001", "IDLE").errback{ |e| a << e }
+      @command_sender.send_command_object(command)
+      a.map(&:class).should == [Errno::ECONNRESET]
+    end
+  end
+
+  describe EM::Imap::CommandSender::LineBuffer do
+
+    it "should not send anything until the buffer is full" do
+      @command_sender.should_not_receive(:send_data)
+      @command_sender.send_line_buffered "RUBY0001"
+    end
+
+    it "should send the entire line including CRLF" do
+      @command_sender.should_receive(:send_data).with("RUBY0001 NOOP\r\n")
+      @command_sender.send_line_buffered "RUBY0001"
+      @command_sender.send_line_buffered " "
+      @command_sender.send_line_buffered "NOOP"
+      @command_sender.send_line_buffered "\r\n"
+    end
+
+    it "should send each line individually" do
+      @command_sender.should_receive(:send_data).with("RUBY0001 NOOP\r\n")
+      @command_sender.should_receive(:send_data).with("RUBY0002 NOOP\r\n")
+      @command_sender.send_line_buffered "RUBY0001 NOOP\r\nRUBY0002 NOOP\r\n"
+    end
   end
 
 end
